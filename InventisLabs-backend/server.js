5b68e2c24124ea0a4035f7bb605eb3ed5df71de7
inventislabs <shaswat@inventislabs.com>
2026-01-22 14:30:23 +0530

    feat: Update allowed origins in server configuration and change API URL in environment file

--- InventisLabs-backend/server.js
+++ InventisLabs-backend/server.js
@@ -65,6 +65,7 @@ const corsOptions = {
 
     const allowedOrigins = [
       process.env.FRONTEND_URL || "http://localhost:5173",
+      "https://www.inventislabs.com",
       "http://localhost:3000",
       "http://localhost:5173",
     ];
